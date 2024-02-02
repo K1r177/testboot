bc5370a029da87fb10e960c5bccbc7675f0e8871
K1r177 <K1r177@yandex.ru>
2024-02-02 10:17:21 +0300

    start

---
+++ catalog/language/ru-ru/extension/module/cats.php
@@ -0,0 +1,5 @@
+<?php
+// Heading
+$_['heading_title'] = 'Категории';
+$_['text_total_products'] = 'Товаров';
+
---
+++ catalog/language/ru-ru/extension/module/featured.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'Рекомендуемые';
+$_['featured_title'] = 'Рекомендуемые товары';
+$_['recent_title'] = 'Новые товары';
+
+// Text
+$_['text_tax']      = 'Без НДС:';
+
